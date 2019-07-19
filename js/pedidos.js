73e4c62cceba85efa490c651c828a1905dd83b45
Guido Mars <gmarsico@conexia.com>
2019-07-19 15:46:12 -0300

    Agrego metodo para pegar a la api Agrego las validaciones de los campos y agrego mensajes de validaciones y error

--- js/pedidos.js
+++ js/pedidos.js
@@ -2,29 +2,87 @@ var pedidos = pedidos || {}
 var url = "localhost:8080/pedidos";
 
  pedidos.vista = (function (){
-
+     var mensajes;
     function init(){
         console.log("Comienza Pedidos")
         limpiarCampos();
     }
 
     function crearPedido(){
-        if(validarCampos){
+        if(validarCampos()){
             pedidos.service.enviarPedido();
+        }else{
+            mostrarValidaciones();
         }
     }
 
     function validarCampos(){
-        return true;
+        let nombre = document.getElementById("nombre").value;
+        let monto = document.getElementById("monto").value;
+        let esValido =true
+        mensajes = [];
+
+        if(nombre === "" ){
+            $('#nombre').addClass('error');
+            mensajes.push("El campo nombre es obligatorio.");
+            esValido = false;
+        }else if (nombre.length > 100) {
+            $('#nombre').addClass('error');
+            errores.push("El campo nombre no puede ser mayor a 100 caracteres.");
+            esValido = false;
+        }
+
+        if (monto === "" ){
+            $('#monto').addClass('error');
+            mensajes.push("El campo monto es obligatorio.");
+            esValido= false;
+        }
+
+        return esValido;
+    }
+
+     function mostrarValidaciones() {
+         mensajes.forEach(function (value, index, array) {
+             $('#div-mensajes').append("<p>"+ value+"</p>");
+         });
+         $('#div-mensajes').addClass('alert-warning');
+         $('#div-mensajes').removeClass('oculto');
+    }
+
+    function mostrarError(error){
+        $('#div-mensajes').append("<p> Ocurrio un error al intentar crear el pedido</p>");
+        $('#div-mensajes').addClass('alert-danger');
+        $('#div-mensajes').removeClass('oculto');
+    }
+
+    function mostrarExito(){
+        $('#div-mensajes').append("<p> Se creo el pedido exitosamente! </p>");
+        $('#div-mensajes').addClass('alert-success');
+        $('#div-mensajes').removeClass('oculto');
     }
 
     function limpiarCampos(){
-
+        document.getElementById("nombre").innerHTML = "";
+        document.getElementById("monto").innerHTML = "";
+        document.getElementById("descuento").innerHTML = "";
     }
+
+    function resetMensajes(){
+        $('input').removeClass('error');
+        $('#div-mensajes').addClass('oculto');
+        $('#div-mensajes').empty()
+        $('#div-mensajes').removeClass('alert-danger');
+        $('#div-mensajes').removeClass('alert-warning');
+        $('#div-mensajes').removeClass('alert-success');
+    }
+
 
     return{
         init:init,
-        crearPedido:crearPedido
+        crearPedido:crearPedido,
+        resetMensajes: resetMensajes,
+        mostrarExito: mostrarExito,
+        mostrarError: mostrarError
     }
 }());
 
@@ -32,12 +90,8 @@ var url = "localhost:8080/pedidos";
 pedidos.service = (function (){
 
     function enviarPedido(){
-
-        pedido = {
-            "nombre": "ps4",
-            "monto":"10",
-            "descuento":"5"
-            }
+        pedidos.vista.resetMensajes();
+        pedido = generarPedido();
 
         $.ajax({
             method: "POST",
@@ -45,10 +99,22 @@ pedidos.service = (function (){
             data: pedido
           })
             .done(function( resultado ) {
-              
+                  if(resultado){
+                    console.log('Exito');
+                      pedidos.vista.mostrarExito()
+                  }else{
+                      pedidos.vista.mostrarError();
+                  }
+            }).fail(function (error) {
+                pedidos.vista.mostrarError();
             });
     }
 
+    function generarPedido(){
+        return { "nombre": document.getElementById("nombre").value,
+            "monto": document.getElementById("monto").value,
+            "descuento": document.getElementById("descuento").value}
+    }
    
     return{
         enviarPedido:enviarPedido
